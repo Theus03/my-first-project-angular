bc6c1b4d532c0b368869eca68a7a997b745a1e51
Matheus Leite <matheus.leite@3stecnologia.com.br>
2025-01-30 19:36:34 -0300

    Passando o param para a service.

--- src/app/services/envia-formulario.service.ts
+++ src/app/services/envia-formulario.service.ts
@@ -8,6 +8,6 @@ export class EnviaFormularioService {
   constructor() { }
 
   enviarInformacaoParaBackEnd(info: string){
-    console.log("enviado para o backend...")
+    console.log(info)
   }
 }
