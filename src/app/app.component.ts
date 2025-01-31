76a96170f72e54ba3feffbc86418b52996dac050
Matheus Leite <matheus.leite@3stecnologia.com.br>
2025-01-31 18:57:16 -0300

    Passando informações entre componentes com Input e Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { HomeComponent } from './components/home/home.component';
 })
 export class AppComponent {
   title = 'meu-primeiro-app';
+
+  logar(event: string) {
+    console.log(event);
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { EnviaFormularioService } from '../../services/envia-formulario.service';
 
 @Component({
@@ -17,11 +17,18 @@ export class HomeComponent {
     this.meuBooleano = valor;
   }
 
+  
+  deveMostrarTitulo = true
+  listItems = ["1", "2", "3"];
+  
+  @Input() minhaProposDeFora!: string;
+  
+  @Output() emitindoValorName = new EventEmitter<string>();
+
   submit(param: any) {
     //console.log("vídeo", param);
+    this.emitindoValorName.emit(this.name);
     this.enviarFormularioService.enviarInformacaoParaBackEnd("enviando para o backend");
   };
 
-  deveMostrarTitulo = true
-  listItems = ["1", "2", "3"];
 }
